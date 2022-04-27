1660e970284167fee241d5bd12e7b0addc1070a7
Harika-Devarapalli <devarapalliharika01@gmail.com>
2022-04-27 20:46:10 +0530

    selecting the restaurant adding to the cart

--- app/src/main/java/com/example/search_project_final/RestarunantMenu.java
+++ app/src/main/java/com/example/search_project_final/RestarunantMenu.java
@@ -1,14 +1,56 @@
 package com.example.search_project_final;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class RestarunantMenu extends AppCompatActivity {
+import com.example.search_project_final.adapaters.MenuListAdapter;
+import com.example.search_project_final.model.Menu;
+import com.example.search_project_final.model.RestaurantModel;
+
+import java.util.List;
+
+public class RestarunantMenu extends AppCompatActivity implements MenuListAdapter.MenuListClickListener{
+    private List<Menu>menuList=null;
+    private MenuListAdapter menuListAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restarunant_menu);
+        RestaurantModel restaurantModel=getIntent().getParcelableExtra("RestaurantModel");
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setTitle(restaurantModel.getName());
+        actionBar.setSubtitle(restaurantModel.getAddress());
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        menuList=restaurantModel.getMenu();
+        initReyclerView();
+
+        TextView submit=findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+    }
+    private void initReyclerView()
+    {
+        RecyclerView recyclerView=findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        menuListAdapter=new MenuListAdapter(menuList,this);
+
+        recyclerView.setAdapter(menuListAdapter);
+    }
+
+    @Override
+    public void onAddToCart(Menu menu) {
+
     }
 }
---
+++ app/src/main/java/com/example/search_project_final/adapaters/MenuListAdapter.java
@@ -0,0 +1,89 @@
+package com.example.search_project_final.adapaters;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.search_project_final.R;
+import com.example.search_project_final.model.Menu;
+import com.example.search_project_final.model.RestaurantModel;
+
+import java.util.List;
+
+public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.MyViewHolder> {
+    private List<Menu> menuList;
+    private MenuListClickListener ClickListener;
+    public MenuListAdapter(List<Menu> menuList, MenuListClickListener clickListener)
+    {
+         this.menuList=menuList;
+         this.ClickListener=clickListener;
+    }
+    public void updateData(List<Menu> menuList)
+    {
+        this.menuList=menuList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public MenuListAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.menu_recycler_row,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MenuListAdapter.MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        holder.menuName.setText(menuList.get(position).getItem_name());
+        holder.menuprice.setText("Price is:" +menuList.get(position).getItem_price());
+        holder.addtocartbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+       //holder.image.setImageBitmap(bi);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClickListener.onAddToCart(menuList.get(position));
+            }
+        });
+        Glide.with(holder.image)
+                .load(menuList.get(position).getUrl())
+                .into(holder.image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return menuList.size();
+    }
+    static class MyViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView menuName;
+        TextView menuprice;
+        TextView addtocartbutton;
+        ImageView image;
+
+
+        public MyViewHolder(View view)
+        {
+            super(view);
+            menuName=view.findViewById(R.id.menuName);
+            menuprice=view.findViewById(R.id.menuprice);
+            addtocartbutton=view.findViewById(R.id.addtocartbutton);
+            image=view.findViewById(R.id.image);
+        }
+    }
+    public interface MenuListClickListener
+    {
+        public void onAddToCart(Menu menu);
+
+    }
+}
